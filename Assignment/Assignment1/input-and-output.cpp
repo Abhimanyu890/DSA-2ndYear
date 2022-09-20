3654d2d885d5bc5c6713ea7355c5c6c2b856f144
Rahul Singh Aswal <91177958+RahulSinghAswal@users.noreply.github.com>
2022-09-21 01:20:49 +0530

    Create input-and-output.cpp

---
+++ Assignment/Assignment1/input-and-output.cpp
@@ -0,0 +1,17 @@
+// HackerRank - https://www.hackerrank.com/challenges/cpp-input-and-output/submissions/code/289844057
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int num1, num2, num3;
+    cin >> num1 >> num2 >> num3;
+    cout << num1 + num2 + num3 ;  
+    return 0;
+}
