da649afc65f18b17de0642c7ed5851eaed7cdcc3
Rahul Singh Aswal <91177958+RahulSinghAswal@users.noreply.github.com>
2022-09-21 01:03:04 +0530

    Update helloworld.cpp

--- Assignment/Assignment1/helloworld.cpp
+++ Assignment/Assignment1/helloworld.cpp
@@ -1,1 +1,10 @@
+// HackerRank https://www.hackerrank.com/challenges/cpp-hello-world/submissions/code/289834125
 
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
